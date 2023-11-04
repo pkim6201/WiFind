3405cf10da8361e81f024d9b08ac12560639dddc
Name <ahmad.awaab@gmail.com>
2023-11-04 15:54:16 -0400

    Setup Stripe SDK

--- app/src/main/java/com/example/wifind/App.kt
+++ app/src/main/java/com/example/wifind/App.kt
@@ -6,6 +6,7 @@ import com.example.wifind.model.Transaction
 import com.example.wifind.model.Wifi
 import com.parse.Parse
 import com.parse.ParseObject
+import com.stripe.android.PaymentConfiguration
 
 class App : Application() {
     override fun onCreate() {
@@ -21,6 +22,10 @@ class App : Application() {
                 .clientKey(getString(R.string.back4app_client_key))
                 .server(getString(R.string.back4app_server_url))
                 .build()
+        )
+        PaymentConfiguration.init(
+            applicationContext,
+            "pk_test_51GrSuPCwLbRg8FLANkdO9QhAoYhvds16JDdIc6decB2PdOKXsNnGAgYKSCjw3n9AfIh3Kn2sNqI6ExMb6jLUl6wr00BfuIrQ6H"
         )
     }
 }
