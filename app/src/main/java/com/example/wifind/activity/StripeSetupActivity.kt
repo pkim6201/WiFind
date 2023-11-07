bd759921fdaad26805ae8914acef4db9681f14ce
Name <ahmad.awaab@gmail.com>
2023-11-07 10:03:04 -0500

    Added bottomnav to StripeSetupActivity

--- app/src/main/java/com/example/wifind/activity/StripeSetupActivity.kt
+++ app/src/main/java/com/example/wifind/activity/StripeSetupActivity.kt
@@ -1,23 +1,41 @@
 package com.example.wifind.activity
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.browser.customtabs.CustomTabsIntent
 import com.example.wifind.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.parse.ParseCloud
 import com.parse.ParseUser
 
 class StripeSetupActivity : AppCompatActivity() {
 
     private lateinit var btOpenStripe: Button
+    private lateinit var bottomNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stripe_setup)
 
         btOpenStripe = findViewById(R.id.bt_open_stripe)
+        bottomNav = findViewById(R.id.bottom_nav)
+
+        bottomNav.selectedItemId = R.id.marketplace
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.marketplace -> true
+                R.id.settings -> {
+                    startActivity(Intent(this, SettingsActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    true
+                }
+
+                else -> false
+            }
+        }
 
         btOpenStripe.setOnClickListener {
             val sellerId = ParseUser.getCurrentUser().fetchIfNeeded().objectId
@@ -29,5 +47,10 @@ class StripeSetupActivity : AppCompatActivity() {
                 CustomTabsIntent.Builder().build().launchUrl(this, Uri.parse(url))
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        bottomNav.selectedItemId = R.id.marketplace
     }
 }
